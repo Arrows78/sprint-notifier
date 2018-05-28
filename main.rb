ccd4ed7b2e1634a51c33a4943d6775fb2feb7d37
Thomas Jost <thomas.jost@jobteaser.com>
2018-05-28 09:49:49 +0200

    Update main script and template format

--- main.rb
+++ main.rb
@@ -1,141 +1,220 @@
 require "erb"
 require "date"
+require 'pathname'
 
 $tickets_list = []
 
-email = {
-  title: "Career Services Sprint Update",
-  sprint_end_date: "#{(Time.now).strftime("%B")} #{Time.now.day}, #{Time.now.year}",
-  edito: "“Hi everybody,<br>This week, we'll have Sacha, Vlatka and Florent working on the sprint.<br>Have a nice day !“<br><br>Christophe and Laurent",
-  witty_comment: "Thomas P. “Tu quoque mi fili“",
-  first_section: {
-    title: "Tickets delivered during last sprint",
-    statuses: ['DONE'],
-    file: 'last_sprint.txt',
-    new_features: {
-      type: "New Feature",
-      nb_tickets: 0,
-      tickets: []
+email_body = {
+    title: "Career Services Sprint Update",
+    sprint_end_date: "#{(Time.now).strftime("%B")} #{Time.now.day}, #{Time.now.year}",
+    edito: "“Hi everybody,<br>This week, we'll have Sacha, Vlatka and Emmanuel working on the sprint.<br>Have a nice day !“<br /><br /><em>Thomas and Laurent</em>",
+    witty_comment: "Delivery Jost-in-Time",
+    first_section: {
+        title: "Tickets delivered during last sprint",
+        statuses: ['DONE'],
+        file: 'last_sprint.txt',
+        new_features: {
+            type: "New Feature",
+            nb_tickets: 0,
+            tickets: []
+        },
+        improvements: {
+            type: "Improvement",
+            nb_tickets: 0,
+            tickets: []
+        },
+        tasks: {
+            type: "Task",
+            nb_tickets: 0,
+            tickets: []
+        },
+        bugs: {
+            type: "Bug",
+            nb_tickets: 0,
+            tickets: []
+        },
     },
-    improvements: {
-      type: "Improvement",
-      nb_tickets: 0,
-      tickets: []
+    second_section: {
+        title: "Tickets not delivered during last sprint <br />(will be transferred to next sprint)",
+        statuses: ['TO DO', 'IN DEV', 'TECH REVIEW', 'FUNCTIONAL REVIEW', 'READY FOR RELEASE'],
+        file: 'last_sprint.txt',
+        new_features: {
+            type: "New Feature",
+            nb_tickets: 0,
+            tickets: []
+        },
+        improvements: {
+            type: "Improvement",
+            nb_tickets: 0,
+            tickets: []
+        },
+        tasks: {
+            type: "Task",
+            nb_tickets: 0,
+            tickets: []
+        },
+        bugs: {
+            type: "Bug",
+            nb_tickets: 0,
+            tickets: []
+        },
     },
-    tasks: {
-      type: "Task",
-      nb_tickets: 0,
-      tickets: []
-    },
-    bugs: {
-      type: "Bug",
-      nb_tickets: 0,
-      tickets: []
-    },
-  },
-  second_section: {
-    title: "Tickets not delivered during last sprint (will be transferred to next sprint)",
-    statuses: ['TO DO','IN DEV','TECH REVIEW','FUNCTIONAL REVIEW', 'READY FOR RELEASE'],
-    file: 'last_sprint.txt',
-    new_features: {
-      type: "New Feature",
-      nb_tickets: 0,
-      tickets: []
-      },
-    improvements: {
-      type: "Improvement",
-      nb_tickets: 0,
-      tickets: []
-    },
-    tasks: {
-      type: "Task",
-      nb_tickets: 0,
-      tickets: []
-    },
-    bugs: {
-      type: "Bug",
-      nb_tickets: 0,
-      tickets: []
-    },
-  },
-  third_section: {
-    title: "Other tickets planned for next sprint",
-    statuses: ['TO DO','IN DEV','TECH REVIEW','FUNCTIONAL REVIEW', 'READY FOR RELEASE','DONE'],
-    file: 'next_sprint.txt',
-    new_features: {
-      type: "New Feature",
-      nb_tickets: 0,
-      tickets: []
-    },
-    improvements: {
-      type: "Improvement",
-      nb_tickets: 0,
-      tickets: []
-    },
-    tasks: {
-      type: "Task",
-      nb_tickets: 0,
-      tickets: []
-    },
-    bugs: {
-      type: "Bug",
-      nb_tickets: 0,
-      tickets: []
+    third_section: {
+        title: "Other tickets planned for next sprint",
+        statuses: ['TO DO', 'IN DEV', 'TECH REVIEW', 'FUNCTIONAL REVIEW', 'READY FOR RELEASE'],
+        file: 'next_sprint.txt',
+        new_features: {
+            type: "New Feature",
+            nb_tickets: 0,
+            tickets: []
+        },
+        improvements: {
+            type: "Improvement",
+            nb_tickets: 0,
+            tickets: []
+        },
+        tasks: {
+            type: "Task",
+            nb_tickets: 0,
+            tickets: []
+        },
+        bugs: {
+            type: "Bug",
+            nb_tickets: 0,
+            tickets: []
+        }
     }
-  }
 }
 
-def section_looper(email)
-  section = [:first_section, :second_section, :third_section]
-  section.each do |section|
-    type_looper(email, section)
-  end
-end
+################################################################################################ FUNCTIONS SECTION
 
-def type_looper(email, section)
-  type = [:new_features, :improvements, :tasks, :bugs]
-  type.each do |type|
-    tickets_looper(email, section, email[section][type][:type], email[section][:file], email[section][:statuses], email[section][type][:tickets], email[section][type])
-  end
-end
+###############################################################################################
+# FUNCTION: section_looper
+# 
+# PURPOSE: Iterate the process on all sections
+# 
+# PARAMETERS:
+#       email_body: Get content of the email to generate
+#
+# RETURNS:  /
+################################################################################################
+def section_looper(email_body)
+    section = [:first_section, :second_section, :third_section]
 
-def tickets_looper(email, section, type, file, statuses, tickets, lol)
-  i = 0
-  File.open(file).readlines.each do |line|
-    if line.include?(type) && statuses.inject(false) { |memo, status| line.include?(status) || memo }
-      unless $tickets_list.include?(line.split("\t")[1])
-      tickets << line.split("\t")[1]
-      i = i + 1
-      end
+    section.each do |section|
+        type_looper(email_body, section)
     end
-  end
-
-  if i>0
-    $tickets_list = tickets + ($tickets_list - tickets)
-    lol[:nb_tickets] = i
-  end
 end
 
-def pluralize(string, n)
-  if n > 1
-    return "#{string}s"
-  else
-    return "#{string}"
-  end
+###############################################################################################
+# FUNCTION: type_looper
+# 
+# PURPOSE: Itearate the process on all type of tickets
+# 
+# PARAMETERS:
+#       email_body: Get content of the email to generate
+#       section: Current section to iterate on
+#
+# RETURNS:  /
+################################################################################################
+def type_looper(email_body, section)
+    type = [:new_features, :improvements, :tasks, :bugs]
+    
+    type.each do |cType|
+        tickets_looper(email_body, section, email_body[section][cType][:type], email_body[section][:file], email_body[section][:statuses], email_body[section][cType][:tickets], email_body[section][cType])
+    end
 end
 
-section_looper(email)
-puts email
+###############################################################################################
+# FUNCTION: tickets_looper
+# 
+# PURPOSE: Iterate on all lines of sprint files (last and next sprints)
+# 
+# PARAMETERS:
+#       email_body: Get content of the email to generate
+#       section: Current section to iterate on
+#       type: ?
+#       file: ?
+#       statuses: ?
+#       tickets: ?
+#       ticket_details: ?
+#
+# RETURNS:  /
+################################################################################################
+def tickets_looper(email_body, section, type, file, statuses, tickets, ticket_details)
+    i = 0
 
-@email = email
-#puts mail_builder()
+    File.open(file).readlines.each do |cLine|
+        # Check if the file has been added
+        if cLine.include?(type) && statuses.inject(false) { |memo, status| cLine.downcase.include?(status.downcase) || memo }
+            unless $tickets_list.include?(cLine.split("\t")[1])
+                tickets << cLine.split("\t")[1]
+                i = i + 1
+            end
+        end
+    end
 
-# render template
+    if i > 0
+        $tickets_list = tickets + ($tickets_list - tickets)
+        ticket_details[:nb_tickets] = i
+    end
+end
+
+###############################################################################################
+# FUNCTION: pluralize
+# 
+# PURPOSE: Add the pluralize
+# 
+# PARAMETERS:
+#       word: The word to check if we need to add plural of not
+#       nb: Number to identify plural
+#
+# RETURNS:  Current string with adjustment on plural word
+################################################################################################
+def pluralize(word, nb)
+    if (nb > 1)
+        return "#{word}s"
+    else
+        return "#{word}"
+    end
+end
+
+###############################################################################################
+# FUNCTION: getfilename
+# 
+# PURPOSE:  Determine the filename with current date
+# 
+# PARAMETERS: /
+# 
+# RETURNS:  Path of the file to create serving to add all information of the sprint update
+################################################################################################
+def getfilename()
+    current_time = Time.new.strftime("%Y-%m-%d")
+    
+    # Create filename
+    filename = 'sprint_update_' + current_time + ".html"
+
+    # Create folder with all file of sprint update
+    foldername = "History"
+    Dir.mkdir(foldername) unless File.exists?(foldername)
+
+    return File.join(".", foldername, filename)
+end
+
+
+################################################################################################ MAIN SECTION
+
+section_looper(email_body)
+puts email_body
+
+@email_body = email_body
+
+# Render template
 template = File.read('./template.html.erb')
 result = ERB.new(template).result(binding)
 
-# write result to file
-# File.open('filename.html', 'w+') do |f|
-File.open('filename.html', 'w+') do |f|
-  f.write result
+# Write result to file
+filename = getfilename()
+File.open(filename, 'w+') do |f|
+    f.write result
 end
